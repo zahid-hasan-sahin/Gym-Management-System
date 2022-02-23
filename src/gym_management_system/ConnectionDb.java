6e9f542f39de6a662f5bdd150cb8f22ee7cd3c25
zahid hasan <zahidhasan6112@gmail.com>
2022-02-23 19:34:02 +0600

    Get name and getAllGmails working perfectly

--- src/gym_management_system/ConnectionDb.java
+++ src/gym_management_system/ConnectionDb.java
@@ -1,6 +1,7 @@
 package gym_management_system;
 
 import java.sql.*;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 public class ConnectionDb {
@@ -41,5 +42,48 @@ public class ConnectionDb {
 
         }
         return false;
+    }
+
+    public static ArrayList<String> getGmailList() {
+        String url = "jdbc:mysql://localhost:3306/gym_management_system";
+        String userName = "root";
+        String password = "";
+        String q = "select gmail from member;";
+        try {
+            Connection con = DriverManager.getConnection(url, userName, password);
+            Statement st = con.createStatement();
+            ResultSet res = st.executeQuery(q);
+            res.next();
+            ArrayList<String> ar = new ArrayList<String>();
+            while (res.next()) {
+                ar.add(res.getString("gmail"));
+            }
+            return ar;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error With Database");
+
+        }
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/gym_management_system";
+        String userName = "root";
+        String password = "";
+        String gmail = "sahin@gmail.com";
+        String q = "select name from member where gmail='" + gmail + "'";
+        try {
+            Connection con = DriverManager.getConnection(url, userName, password);
+            Statement st = con.createStatement();
+            ResultSet res = st.executeQuery(q);
+            res.next();
+            System.out.println(res.getString("name"));
+        } catch (Exception e) {
+
+            JOptionPane.showMessageDialog(null, "Error With Database");
+
+        }
+
     }
 }
--- src/gym_management_system/MemberInfo.java
+++ src/gym_management_system/MemberInfo.java
@@ -8,103 +8,103 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MemberInfo {
-    
+
     Member mem = new Member();
-    
+
     void addMember(String gmail) throws IOException {
         mem.setGmail(gmail);
     }
-    
+
     void deleteMember(String gmail) throws FileNotFoundException, IOException {
         ArrayList<String> member = getAllMembersGmail();
         member.remove(gmail);
-        
+
         FileWriter fw = new FileWriter("member_Details//gmails.txt");
         for (int i = 0; i < member.size(); i++) {
             fw.write(member.get(i) + "\r\n");
         }
         fw.close();
     }
-    
+
     boolean isExistMember(String gmail) throws FileNotFoundException {
-        
+
         return ConnectionDb.isExistMember(gmail);
-        
+
     }
-    
+
     void setName(String gmail, String name) throws IOException {
         mem.setName(name);
     }
-    
+
     String getName(String gmail) throws IOException {
         File fw = new File("member_Details//" + gmail + "//name.txt");
         Scanner scan = new Scanner(fw);
         return scan.nextLine();
-        
+
     }
-    
+
     void setPhone(String gmail, String phone) throws IOException {
         mem.setPhone(phone);
     }
-    
+
     String getPhone(String gmail) throws IOException {
         File fw = new File("member_Details//" + gmail + "//phone.txt");
         Scanner scan = new Scanner(fw);
         return scan.nextLine();
-        
+
     }
-    
+
     void setAge(String gmail, String age) throws IOException {
         mem.setAge(age);
     }
-    
+
     String getAge(String gmail) throws IOException {
         File fw = new File("member_Details//" + gmail + "//age.txt");
         Scanner scan = new Scanner(fw);
         return scan.nextLine();
-        
+
     }
-    
+
     void setGender(String gmail, String gender) throws IOException {
         mem.setGender(gender);
     }
-    
+
     String getGender(String gmail) throws IOException {
         File fw = new File("member_Details//" + gmail + "//gender.txt");
         Scanner scan = new Scanner(fw);
         return scan.nextLine();
-        
+
     }
-    
+
     void setTime(String gmail, String time) throws IOException {
         mem.setTime(time);
     }
-    
+
     String getTime(String gmail) throws IOException {
         File fw = new File("member_Details//" + gmail + "//time.txt");
         Scanner scan = new Scanner(fw);
         return scan.nextLine();
-        
+
     }
-    
+
     void setAmount(String gmail, String amount) throws IOException {
         mem.setAmount(amount);
     }
-    
+
     String getAmount(String gmail) throws IOException {
         File fw = new File("member_Details//" + gmail + "//amount.txt");
         Scanner scan = new Scanner(fw);
         return scan.nextLine();
-        
+
     }
-    
+
     void setPayment(String gmail, String amount, String date) throws IOException {
-        
+
         FileWriter fw = new FileWriter("member_Details//" + gmail + "//payment.txt", true);
         fw.write(amount + " " + date + "\r\n");
         fw.close();
     }
-    
+
     ArrayList<String> getPayment(String gmail) throws IOException {
         try {
             File fw = new File("member_Details//" + gmail + "//payment.txt");
@@ -113,32 +113,26 @@ public class MemberInfo {
             while (scan.hasNext()) {
                 pay.add(scan.next() + " " + scan.next());
             }
-            
+
             return pay;
         } catch (Exception E) {
             FileWriter fw = new FileWriter("member_Details//" + gmail + "//payment.txt");
             fw.close();
             return new ArrayList<String>();
-            
         }
-        
+
     }
-    
+
     ArrayList<String> getAllMembersGmail() throws FileNotFoundException {
-        
+
         ArrayList<String> members = new ArrayList<String>();
-        
-        File fw = new File("member_Details//gmails.txt");
-        Scanner scan = new Scanner(fw);
-        while (scan.hasNext()) {
-            members.add(scan.nextLine());
-        }
+
         return members;
     }
-    
+
     void addToDb() {
         ConnectionDb.addMember(mem);
         System.out.println();
     }
-    
+
 }
